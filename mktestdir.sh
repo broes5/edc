f308195cee445d2d79effc957b33455dbec605ac
Benjamin Roeser <broes2018@gmail.com>
2024-06-15 13:00:46 +1000

    I want the `mktestdir.sh` script to be the same in both branches.

--- mktestdir.sh
+++ mktestdir.sh
@@ -12,3 +12,8 @@ cd subdir
 touch photo.JPG
 touch image.PNG
 touch document.ODF
+
+#mkdir subdir{0..5000}
+#touch subdir{0..5000}/photo.JPG
+#touch subdir{0..5000}/image.PNG
+#touch subdir{0..5000}/document.ODF
